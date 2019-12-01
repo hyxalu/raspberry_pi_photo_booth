6d2f96775f0a5c18a3d24e116bdc9c6a18641971
david <schaillie@gmail.com>
2019-12-01 09:42:38 +0100

    Add svg files (wedding version)

---
+++ assets/export.bat
@@ -0,0 +1,9 @@
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 intro_1.svg -e intro_1.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 intro_2.svg -e intro_2.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 get_ready_1.svg -e get_ready_1.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 get_ready_2.svg -e get_ready_2.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 get_ready_3.svg -e get_ready_3.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 get_ready_4.svg -e get_ready_4.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 all_done.svg -e all_done.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 all_done_delayed_upload.svg -e all_done_delayed_upload.png
+"C:\Program Files\Inkscape\inkscape.exe" -z -w 800 -h 480 processing.svg -e processing.png
